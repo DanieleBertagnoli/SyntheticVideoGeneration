fa841ae52e11f7e006ae316a3ced5c7f759d0dfb
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2024-04-03 13:20:18 +0200

    Added multithreading

--- Code/SceneGeneration/generate_bboxes.py
+++ Code/SceneGeneration/generate_bboxes.py
@@ -1,7 +1,10 @@
 import os 
-import numpy as np
+from time import time
 from typing import *
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
+import numpy as np
 import trimesh
 import trimesh.transformations as tf
 import cv2
@@ -19,6 +22,7 @@ def get_model_name_from_id(id) -> str:
         str: The name of the model corresponding to the given ID, if found.
              None if no matching model ID is found in the YAML file.
     """
+    id = id + 1 # TODO: remove it
 
     # Construct the path to the YAML file containing model IDs and names
     yaml_path = os.path.join(os.path.dirname(__file__), '..', '..', 'Data', 'Configs', 'models_id.yml')
@@ -37,7 +41,7 @@ def get_model_name_from_id(id) -> str:
 
 
 
-def project_points(points_3d: np.ndarray, blendercam_in_world: np.ndarray, intrinsic_matrix: np.ndarray, img_width: int) -> List[Tuple[float, float]]:
+def project_points(points_3d: np.ndarray, blendercam_in_world: np.ndarray, intrinsic_matrix: np.ndarray, img_width: int, img_heigth:int) -> List[Tuple[float, float]]:
     """
     Projects 3D points onto a 2D image plane.
 
@@ -65,20 +69,34 @@ def project_points(points_3d: np.ndarray, blendercam_in_world: np.ndarray, intri
     points = []
     for p in points_2d:
         x = img_width/2 + (img_width/2 - p[0])
-        points.append([x, p[1]])
+        y = p[1]
+
+        # If the object is not in camera at all, the projected points could assume too negative or too positive values, 
+        # therefore we put a cap to avoid overflows in the next operations
+        if x < -1:
+            x = -1
+        if y < -1:
+            y = -1
+
+        if x > img_width + 1:
+            x = img_width + 1
+        if y > img_heigth + 1:
+            y = img_heigth + 1
+
+        points.append([x, y])
 
     return points
 
 
 
-def get_object_dimensions(model_path, poses, rotation_translation_matrix, intrinsic_matrix, img_width, bbox_adjustment) -> Tuple[float, float]:
+def get_object_dimensions(model_path, poses, blendercam_in_world, intrinsic_matrix, img_width, img_heigth, bbox_adjustment) -> Tuple[float, float]:
     """
     Get the dimensions and projected points of an object in an image.
 
     Args:
         - model_path (str): The file path to the 3D model of the object.
         - poses (np.ndarray): The 4x4 transformation matrix representing the pose of the object in world coordinates.
-        - rotation_translation_matrix (np.ndarray): 
+        - blendercam_in_world (np.ndarray): 
         - intrinsic_matrix (np.ndarray): The camera intrinsic matrix.
         - img_width (int): The width of the image.
         - bbox_adjustment (int): The adjustment value for the bounding box.
@@ -95,8 +113,8 @@ def get_object_dimensions(model_path, poses, rotation_translation_matrix, intrin
     transformed_mesh.apply_transform(poses)
     
     # Project the vertices of the 3D model onto the image
-    projected_points = project_points(transformed_mesh.vertices, rotation_translation_matrix, intrinsic_matrix, img_width)
-    
+    projected_points = project_points(transformed_mesh.vertices, blendercam_in_world, intrinsic_matrix, img_width, img_heigth)
+
     # Calculate the bounding box around the projected points
     bbox_x_min, bbox_y_min = np.min(projected_points, axis=0)
     bbox_x_max, bbox_y_max = np.max(projected_points, axis=0)
@@ -118,12 +136,12 @@ def get_object_dimensions(model_path, poses, rotation_translation_matrix, intrin
 
 
 
-def generated_bboxes(model_paths, rotation_translation_matrix, poses, image_path, model_name, intrinsic_matrix, img_width, bbox_adjustment, cls_ind, show_image=False) -> np.ndarray:
+def generated_bboxes(model_paths, blendercam_in_world, poses, image_path, model_name, intrinsic_matrix, img_width, img_heigth, bbox_adjustment, show_image=False) -> np.ndarray:
     """
     Generate bounding boxes on an image for a given object.
 
     Args:
-        rotation_translation_matrix (numpy.ndarray): Pose transformation matrix of the Blender camera in world coordinates.
+        blendercam_in_world (numpy.ndarray): Pose transformation matrix of the Blender camera in world coordinates.
         poses (numpy.ndarray): Pose transformation matrix of the object in world coordinates.
         image_path (str): Path to the input image.
         model_name (str): Name of the object model.
@@ -141,7 +159,7 @@ def generated_bboxes(model_paths, rotation_translation_matrix, poses, image_path
             break
 
     # Get the bbox
-    bbox, points = get_object_dimensions(model_path, poses, rotation_translation_matrix, intrinsic_matrix, img_width, bbox_adjustment)
+    bbox, points = get_object_dimensions(model_path, poses, blendercam_in_world, intrinsic_matrix, img_width, img_heigth, bbox_adjustment)
 
     if show_image:
 
@@ -186,19 +204,102 @@ def is_box_inside(box, threshold=10) -> bool:
     x1, y1, x2, y2 = box
 
     # Calculate the area of the bounding box
-    box_area = (x2 - x1) * (y2 - y1)
+    box_area = abs(x2 - x1 * y2 - y1)
 
     # Calculate the area of intersection with the image
     inside_width = min(x2, image_width) - max(x1, 0)
     inside_height = min(y2, image_height) - max(y1, 0)
     inside_area = max(inside_width, 0) * max(inside_height, 0)
 
-    # Calculate the percentage of the box inside the image
-    inside_percentage = (inside_area / box_area) * 100
+    if box_area == 0:
+        inside_percentage = 0
+    else:
+        # Calculate the percentage of the box inside the image
+        inside_percentage = (inside_area / box_area) * 100
 
     # Check if the box is inside the image for at least the threshold percentage
     return inside_percentage >= threshold
 
+
+def process_folder(folder_name):
+
+    thread_id = threading.get_ident()
+    folder_name_path = os.path.join(GENERATED_SCENES_PATH, folder_name)
+
+    if not os.path.isdir(folder_name_path):
+        return
+
+    print(f'\n\n --- Generating bboxes for sequence {folder_name} by thread {thread_id} ---\n\n')
+
+    start = time()
+
+    # Iterate through files in the directory
+    for file_name in os.listdir(folder_name_path):
+
+        # Check if the file is an npy file
+        if not file_name.endswith('.npy'):
+            continue
+
+        # Extract scene ID from file name
+        scene_id = file_name.split('-')[0]
+
+        # Load metadata from the file
+        metadata = np.load(os.path.join(folder_name_path, file_name), allow_pickle=True).item()
+
+        bboxes = {}
+        count_object_id = 0
+            
+        # Iterate through class IDs in the metadata
+        new_class_ids = []
+        new_poses = []
+        for class_id in metadata['cls_indexes']:
+
+            # Generate bounding box for the object
+            model_name = get_model_name_from_id(class_id)
+
+            bboxes[model_name] = []
+
+            bbox = generated_bboxes(model_paths,
+                                    metadata['blendercam_in_world'],
+                                    metadata['poses'][count_object_id],
+                                    os.path.join(folder_name_path, f'{scene_id}-color.png'),
+                                    model_name,
+                                    metadata['intrinsic_matrix'], 
+                                    config_file['camera_settings']['width'],
+                                    config_file['camera_settings']['height'],
+                                    config_file['bbox_adjustment'],
+                                    False)
+                
+            (x1, y1, x2, y2) = (bbox[0][0], bbox[0][1], bbox[2][0], bbox[2][1])
+
+            if is_box_inside((x1, y1, x2, y2)):
+                bboxes[model_name].append(bbox)
+                new_class_ids.append(class_id)
+                new_poses.append(metadata['poses'][count_object_id])
+                
+            count_object_id += 1
+
+
+        with open(os.path.join(folder_name_path, f'{scene_id}-box.txt'), 'w') as f:
+            for model in bboxes:
+                for model_instance in bboxes[model]:
+                    bbox_coords = f'{model_instance[0][0]} {model_instance[0][1]} {model_instance[2][0]} {model_instance[2][1]}'
+                    f.write(f"{model[:-4]} {bbox_coords}\n")
+            
+            
+        data_dict = {
+            'cls_indexes': new_class_ids,
+            'poses':new_poses,
+            'blendercam_in_world': metadata['blendercam_in_world'],
+            'intrinsic_matrix': metadata['intrinsic_matrix']
+        }
+
+        # Save the dictionary as a .npy file
+        npy_filename = os.path.join(folder_name_path, file_name)
+        np.save(npy_filename, data_dict)
+
+    delta = int(time() - start)
+    print(f'Box generation for sequence {folder_name} done in {delta} seconds by thread {thread_id}')
 
 
 if __name__ == '__main__':
@@ -225,75 +326,8 @@ if __name__ == '__main__':
 
     folder_list = os.listdir(GENERATED_SCENES_PATH)
     folder_list.sort()
-    for folder_name in folder_list:
-        folder_name_path = os.path.join(GENERATED_SCENES_PATH, folder_name)
 
-        if not os.path.isdir(folder_name_path):
-            continue
-
-        print(f'\n\n --- Generating bboxes for sequence {folder_name} ---\n\n')
-
-        # Iterate through files in the directory
-        for file_name in sorted(os.listdir(folder_name_path)):
-
-            # Check if the file is an npy file
-            if not file_name.endswith('.npy'):
-                continue
-
-            # Extract scene ID from file name
-            scene_id = file_name.split('-')[0]
-
-            # Load metadata from the file
-            metadata = np.load(os.path.join(folder_name_path, file_name), allow_pickle=True).item()
-
-            bboxes = {}
-            count_object_id = 0
-            
-            # Iterate through class IDs in the metadata
-            new_class_ids = []
-            new_poses = []
-            for class_id in metadata['cls_indexes']:
-
-                # Generate bounding box for the object
-                model_name = get_model_name_from_id(class_id)
-
-                bboxes[model_name] = []
-
-                bbox = generated_bboxes(model_paths,
-                                        metadata['blendercam_in_world'],
-                                        metadata['poses'][count_object_id],
-                                        os.path.join(folder_name_path, f'{scene_id}-color.png'),
-                                        model_name,
-                                        metadata['intrinsic_matrix'], 
-                                        config_file['camera_settings']['width'],
-                                        config_file['bbox_adjustment'],
-                                        metadata['cls_indexes'],
-                                        True)
-                
-                (x1, y1, x2, y2) = (bbox[0][0], bbox[0][1], bbox[2][0], bbox[2][1])
-
-                if is_box_inside((x1, y1, x2, y2)):
-                    bboxes[model_name].append(bbox)
-                    new_class_ids.append(class_id)
-                    new_poses.append(metadata['poses'][count_object_id])
-                
-                count_object_id += 1
-
-
-            with open(os.path.join(folder_name_path, f'{scene_id}-box.txt'), 'w') as f:
-                for model in bboxes:
-                    for model_instance in bboxes[model]:
-                        bbox_coords = f'{model_instance[0][0]} {model_instance[0][1]} {model_instance[2][0]} {model_instance[2][1]}'
-                        f.write(f"{model[:-4]} {bbox_coords}\n")
-            
-            
-            data_dict = {
-                'cls_indexes': new_class_ids,
-                'poses':new_poses,
-                'blendercam_in_world': metadata['blendercam_in_world'],
-                'intrinsic_matrix': metadata['intrinsic_matrix']
-            }
-
-            # Save the dictionary as a .npy file
-            npy_filename = os.path.join(folder_name_path, file_name)
-            np.save(npy_filename, data_dict)
+    # Use ThreadPoolExecutor to process each folder in parallel
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        # Map the process_folder function to each folder in the folder_list
+        results = list(executor.map(process_folder, folder_list))
